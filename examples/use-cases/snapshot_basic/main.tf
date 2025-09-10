46dca7712f3f8c8b8ca489dfe7adfb7dae9b70b9
matttrach <matt.trachier@suse.com>
2025-09-10 10:11:58 -0500

    fix: remove unused variables in snapshot example
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- examples/use-cases/snapshot_basic/main.tf
+++ examples/use-cases/snapshot_basic/main.tf
@@ -1,5 +1,4 @@
 
-
 provider "file" {}
 
 locals {
--- examples/use-cases/snapshot_basic/variables.tf
+++ examples/use-cases/snapshot_basic/variables.tf
@@ -1,15 +1,4 @@
 
-
-variable "directory" {
-  type    = string
-  default = "."
-}
-
-variable "name" {
-  type    = string
-  default = "snapshot_use_case_test.txt"
-}
-
 variable "update" {
   type    = string
   default = "code-change-necessary"
