136b3f112dd30dfb92bb7060de8f695a475394f4
Matt Trachier <matt.trachier@suse.com>
2026-07-28 12:04:16 -0500

    fix: update goreleaser config with some troubleshooting information (#344)
    Signed-off-by: Matt Trachier <matt.trachier@suse.com>

--- .github/workflows/scripts/goreleaser.sh
+++ .github/workflows/scripts/goreleaser.sh
@@ -15,15 +15,20 @@ if [[ -z "${GPG_KEY:-}" ]]; then echo "Error: GPG_KEY is empty" >&2; exit 1; fi
 echo "Importing GPG key..."
 echo "${GPG_KEY}" | gpg --import --batch > /dev/null || { echo "Error: Failed to import GPG key" >&2; exit 1; }
 
-if [[ -n "${WORKING_DIR:-}" ]]; then
-  cd "${WORKING_DIR}"
+# https://www.gnupg.org/documentation/manuals/gnupg24/gpg.1.html
+# https://goreleaser.com/customization/sign/sign/
+# troubleshooting information
+gpg --version
+# this only lists UIDs no secret material
+gpg --batch --list-secret-keys --keyid-format LONG
+# this will fail if the secret key isn't present
+gpg --batch --list-secret-keys --keyid-format LONG "${GPG_KEY_ID}" >/dev/null
+# troubleshooting information
+goreleaser --version
+
+if [ -f .goreleaser.yml ]; then
+  goreleaser release --clean --config .goreleaser.yml
+else
+  echo "Error: .goreleaser.yml not found" >&2
+  exit 1
 fi
-
-ARGS=("--clean")
-if [[ "${SKIP_VALIDATE:-false}" == "true" ]]; then
-  ARGS+=("--skip=validate")
-fi
-
-ARGS+=("--config" "${GORELEASER_CONFIG:-.goreleaser.yml}")
-
-goreleaser release "${ARGS[@]}"
