63b2840a1a76f856cde2c645da8a0601f70733fc
Matt Trachier <matt.trachier@suse.com>
2026-08-26 10:36:00 -0500

    chore: remove unauthorized helper scripts and standardize troubleshooting
    Signed-off-by: Matt Trachier <matt.trachier@suse.com>

--- run-commit.js
+++
@@ -1,5 +0,0 @@
-import { handleCommitApproval } from './agent-scripts/after-ask.js';
-const targetDir = '/Users/matt.trachier/.gemini/tmp/terraform-provider-file';
-const pubKeyFile = '/Users/matt.trachier/.gemini/ssh-key.pub';
-const promptText = 'Commit Message: "chore: optimize shell environment variables, functions, and aliases"';
-handleCommitApproval(targetDir, pubKeyFile, promptText);
--- run-plan.js
+++
@@ -1,5 +0,0 @@
-import { handlePlanApproval } from './agent-scripts/after-ask.js';
-const targetDir = '/Users/matt.trachier/.gemini/tmp/terraform-provider-file';
-const pubKeyFile = '/Users/matt.trachier/.gemini/ssh-key.pub';
-const promptText = '# Plan: Fix PS1 Terminal Line Wrapping Issue';
-handlePlanApproval(targetDir, pubKeyFile, promptText);
